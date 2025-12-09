b5ee1759417f25400a6cae78cd897d64c891090f
Marco Ferreira <marco.ferreira@outsystems.com>
2025-12-09 13:58:57 +0000

    Added Conditionals

---
+++ Week1_Basics/Conditionals/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Indique a idade: ");
+string idade = Console.ReadLine();
+int idade2 = int.Parse(idade);
+
+if (idade2 >= 18)
+{
+    Console.WriteLine("Adulto");
+}
+else if (idade2 >= 13)
+{
+    Console.WriteLine("Adolescente");
+}
+else
+{
+    Console.WriteLine("Criança");
+}
+
+
+// Necessidade de receber primeiro o Console.ReadLine(); como string e converter a int antes de fazer as comparaçoes.
