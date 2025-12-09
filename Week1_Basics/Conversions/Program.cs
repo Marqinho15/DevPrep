8d256532d4f6826cb7196d0a281f8862976bbde7
Marco Ferreira <marco.ferreira@outsystems.com>
2025-12-09 13:41:15 +0000

    Added Conversions

---
+++ Week1_Basics/Conversions/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Insere um numero para duplicar: ");
+string numero = Console.ReadLine();
+int numero1 = int.Parse(numero);
+int dobro = numero1 * 2;
+Console.WriteLine("O dobro do teu número é: " + dobro);
