1e3a1db34e43e6f096028c9e5621cf28ec9738e5
Marco Ferreira <marco.ferreira@outsystems.com>
2025-12-09 14:14:26 +0000

    Added Functions

--- Week1_Basics/Functions/Program.cs
+++ Week1_Basics/Functions/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*Cria função Somar(int a, int b)
+Chama-a com 2 números e imprime resultado
+Cria função Multiplicar com static e imprime
+*/
+
+
+int Somar(int a, int b)
+{
+    return a + b;
+}
+int resultado = Somar(3, 4);
+Console.WriteLine("A soma dá " + (resultado));
+
+static int Multiplicar (int a, int b)
+{
+    return a * b;
+}
+int multiplicacao = Multiplicar(4, 2);
+Console.WriteLine("A Multiplicação dá " + (multiplicacao));
