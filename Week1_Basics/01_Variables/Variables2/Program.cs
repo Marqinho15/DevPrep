3cf1e51ae66743a178b24fb3e74eb5267f220cf6
Marco Ferreira <marco.ferreira@outsystems.com>
2025-12-09 13:33:43 +0000

    Added Variables and Variables2

---
+++ Week1_Basics/01_Variables/Variables2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idade = 29;
+string nome = "Marco";
+double salario = 1234.56;
+bool ativo = true;
+char inicial = 'M';
+Console.WriteLine($"Nome: {nome}, Idade: {idade}, Salário: {salario}, ativo: {ativo}, Inicial: {inicial}");
+
+// copiei a .WriteLine
+// issues no char, estava a usar "" em vez de ''
---
+++ Week1_Basics/01_Variables/VariablesIntro/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idade = 29;
+double salario = 1234.56;
+string nome = "Marco";
+bool temADHD = true;
+Console.WriteLine($"Nome: {nome}, Idade: {idade}, Salário: {salario}, ADHD: {temADHD}");
+
+// resposta dada pelo Gepeto
