1abfc806814f9c80a1db40d1a421ecbcfd8bd0a8
Marco Ferreira <marco.ferreira@outsystems.com>
2025-12-09 14:32:29 +0000

    Added ArraysandLists

---
+++ Week1_Basics/ArraysandLists/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Cria array de 5 números e imprime cada elemento
+Cria uma List<string> com 3 nomes, adiciona mais 1, remove 1, imprime tamanho
+*/
+int[] numeros = {1,2,3,4,5};
+
+foreach (var item in numeros)
+{
+    Console.WriteLine (item);
+}
+
+List<string> lista = new List<string> {"Ana", "Marco", "Beatriz"};
+lista.Add("Leonor");
+lista.Remove("Ana");
+Console.WriteLine(lista.Count);
